d82709f0450ba2da8d14b83c27a05ec432b91aa6
Ajay Digital Dreamworks <autenicyt@gmail.com>
2026-03-06 19:44:27 +0530

    not

---
+++ backend/models/Notification.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  title: String,
+  message: String,
+  type: {
+    type: String,
+    enum: ["streak", "followup", "personal"]
+  },
+  isRead: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Notification", notificationSchema);
